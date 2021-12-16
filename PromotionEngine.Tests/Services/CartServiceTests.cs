17ed2b9aa5fb9bc6d1d3ad017568b25ab1094c80
Raheem Yousaf <raheem.yousaf@hotmail.co.uk>
2021-12-16 15:55:53 +0000

    Added Cart Service Tests

--- PromotionEngine.Tests/Services/CartServiceTests.cs
+++ PromotionEngine.Tests/Services/CartServiceTests.cs
@@ -1,22 +1,162 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using PromotionEngine.Enum;
 using PromotionEngine.Interfaces;
 using PromotionEngine.Models.BusinessModels;
 using PromotionEngine.Models.EntityModels;
 using PromotionEngine.Services;
+using PromotionEngine.Services.PromotionStrategies;
 using PromotionEngine.Tests.Abstracts;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PromotionEngine.Tests.Services
 {
     [TestClass]
     public class CartServiceTests : TestBase
     {
+        private Mock<ILogger<ICartService>> mockLogger { get; set; }
+        private Mock<IAppSettings> mockAppsettings { get; set; }
+        private IPromotionStrategyService promotionStrategyService { get; set; }
         private ICartService cartService { get; set; }
 
         public CartServiceTests()
         {
-            //this.cartService = new CartService()
+            this.mockLogger = new Mock<ILogger<ICartService>>();
+            this.MockPromotionService = new Mock<IPromotionService>();
+            this.mockAppsettings = new Mock<IAppSettings>();
+
+            this.MockPromotionService.Setup(x => x.GetActivePromotions(PromotionType.SingleItem)).Returns(this.MockPromotions().Where(x => x.PromotionType == PromotionType.SingleItem).ToList());
+            this.MockPromotionService.Setup(x => x.GetActivePromotions(PromotionType.MultipleItems)).Returns(this.MockPromotions().Where(x => x.PromotionType == PromotionType.MultipleItems).ToList());
+
+            SingleItemPromotionStrategy singleItemPromotionStrategy = new SingleItemPromotionStrategy(new Mock<ILogger<SingleItemPromotionStrategy>>().Object);
+            MultipleItemPromotionStrategy multipleItemPromotionStrategy = new MultipleItemPromotionStrategy(new Mock<ILogger<MultipleItemPromotionStrategy>>().Object);
+
+            this.promotionStrategyService = new PromotionStrategyService(new Mock<ILogger<IPromotionStrategyService>>().Object, this.MockPromotionService.Object, this.mockAppsettings.Object, singleItemPromotionStrategy, multipleItemPromotionStrategy);
+
+            this.cartService = new CartService(this.promotionStrategyService, this.mockLogger.Object);
+        }
+
+        [TestMethod]
+        public void ApplyPromotion_ScenarioA()
+        {
+            this.mockAppsettings.Setup(x => x.AllowedPromotionType).Returns($"{PromotionType.SingleItem.ToString()},{PromotionType.MultipleItems.ToString()}");
+
+            List<ICartItemModel> cartItems = new List<ICartItemModel>();
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "A",
+                UnitPrice = 50,
+                Quantity = 1,
+                TotalPrice = 50,
+            });
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "B",
+                UnitPrice = 30,
+                Quantity = 1,
+                TotalPrice = 30,
+            });
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "C",
+                UnitPrice = 20,
+                Quantity = 1,
+                TotalPrice = 20,
+            });
+
+            ICartModel cart = new CartModel()
+            {
+                CartItems = cartItems
+            };
+
+            ICartModel updatedCart = this.cartService.CalculateTotalPromotionPrice(cart);
+
+            Assert.AreEqual(100, updatedCart.TotalPrice);
+        }
+
+        [TestMethod]
+        public void ApplyPromotion_ScenarioB()
+        {
+            this.mockAppsettings.Setup(x => x.AllowedPromotionType).Returns($"{PromotionType.SingleItem.ToString()},{PromotionType.MultipleItems.ToString()}");
+
+            List<ICartItemModel> cartItems = new List<ICartItemModel>();
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "A",
+                UnitPrice = 50,
+                Quantity = 5,
+                TotalPrice = 250,
+            });
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "B",
+                UnitPrice = 30,
+                Quantity = 5,
+                TotalPrice = 150,
+            });
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "C",
+                UnitPrice = 20,
+                Quantity = 1,
+                TotalPrice = 20,
+            });
+
+            ICartModel cart = new CartModel()
+            {
+                CartItems = cartItems
+            };
+
+            ICartModel updatedCart = this.cartService.CalculateTotalPromotionPrice(cart);
+
+            Assert.AreEqual(370, updatedCart.TotalPrice);
+        }
+
+        [TestMethod]
+        public void ApplyPromotion_ScenarioC()
+        {
+            this.mockAppsettings.Setup(x => x.AllowedPromotionType).Returns($"{PromotionType.SingleItem.ToString()},{PromotionType.MultipleItems.ToString()}");
+
+            List<ICartItemModel> cartItems = new List<ICartItemModel>();
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "A",
+                UnitPrice = 50,
+                Quantity = 3,
+                TotalPrice = 250,
+            });
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "B",
+                UnitPrice = 30,
+                Quantity = 5,
+                TotalPrice = 150,
+            });
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "C",
+                UnitPrice = 20,
+                Quantity = 1,
+                TotalPrice = 20,
+            });
+            cartItems.Add(new CartItemModel()
+            {
+                SKU = "D",
+                UnitPrice = 15,
+                Quantity = 1,
+                TotalPrice = 15,
+            });
+
+            ICartModel cart = new CartModel()
+            {
+                CartItems = cartItems
+            };
+
+            ICartModel updatedCart = this.cartService.CalculateTotalPromotionPrice(cart);
+
+            Assert.AreEqual(280, updatedCart.TotalPrice);
         }
 
         private List<IPromotionModel> MockPromotions()
